e7309cfda607a3229c24aa5c7310aaafc9264f44
Ksenia Fihas <ksenia.fihas@gmail.com>
2023-03-16 18:28:38 +0100

    task 11

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import axios from "axios";
 import Notiflix from "notiflix";
-import simpleLightbox from "simplelightbox";
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css"
+
 
 const refs = {
     form: document.querySelector(".search-form"),
@@ -45,10 +47,89 @@ function onButtonMore() {
 
 async function pixabay(name, page) {
     const API_URL = 'https://pixabay.com/api/';
-}
+
 
 const options = {
     params: {
         key: "34366428-c8312deaec1e385c65e1239b7",
+        q: name,
+        image_type: "photo",
+        orientation: "horizontal",
+        safesearch: "true",
+        page: page,
+        per_page: 40,
+    },
+};
+
+try {
+    const response = await axios.get(API_URL, options);
+
+    notification(
+        response.data.hits.length,
+        response.data.total
+    );
+
+    createMarkup(response.data); 
+} catch (error) {
+    console.log(error);
     }
+};
+
+
+function createMarkup(arr) {
+    const markup = arr.hits.map(item =>
+        `<a class="photo-link" href="${item.largeImageURL}">
+            <div class="photo-card">
+            <div class="photo">
+            <img src="${item.webformatURL}" alt="${item.tags}" loading="lazy"/>
+            </div>
+                    <div class="info">
+                        <p class="info-item">
+                            <b>Likes</b>
+                            ${item.likes}
+                        </p>
+                        <p class="info-item">
+                            <b>Views</b>
+                            ${item.views}
+                        </p>
+                        <p class="info-item">
+                            <b>Comments</b>
+                            ${item.comments}
+                        </p>
+                        <p class="info-item">
+                            <b>Downloads</b>
+                            ${item.downloads}
+                        </p>
+                    </div>
+            </div>
+        </a>`).join("");
+    refs.gallery.innerHTML("beforeend", markup);
+    simpleLightbox.refresh();
 }
+
+
+const simpleLightbox = new SimpleLightbox(".gallery a", {
+    captionsData: "alt",
+    captionDelay: 250,
+});
+
+function notification(length, totalHits) {
+    if (length === 0) {
+        Notiflix.Notify.failure('Sorry, there are no images matching your search query. Please try again.');
+    
+    return;
+}
+
+
+if (page === 1) {
+    refs.buttonMore.style.display = "flex";
+    Notiflix.Notify.success(`Hooray! We found ${totalHits} images.`);
+}
+
+if (length < 40) {
+    refs.buttonMore.style.display = "none";
+
+    Notiflix.Notify.info("We're sorry, but you've reached the end of search results.");
+    }
+    }
+
